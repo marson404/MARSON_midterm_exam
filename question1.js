7bfd84faf639da0ab2d02a222a7077619f297895
unknown <YY534@ccs.local>
2025-03-15 15:13:18 +0800

    question1.js

---
+++ question1.js
@@ -0,0 +1,5 @@
+Create a new git repository.
+Add a file named git-basic.txt with the content "Write your full name".
+Commit the change with the message "Add git-basic.txt".
+Create a new repository on GitHub and push your local repository to GitHub.
+Hint: Focus on basic git commands: init, add, commit, and push.
